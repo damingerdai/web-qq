f2079e02ddbc6888e7f4f3ba98dbd27648686b2b
Arthur.Ming <arthur.ming@acxiom.com>
2017-10-10 14:07:56 +0800

    add some new method

--- src/main/java/org/aming/web/qq/utils/NumberUtils.java
+++ src/main/java/org/aming/web/qq/utils/NumberUtils.java
@@ -5,7 +5,33 @@ package org.aming.web.qq.utils;
  * @version 2017/10/5.
  */
 public class NumberUtils {
+
+    /**
+     * 判断是否大于零
+     * @param i
+     * @return
+     */
     public static boolean isGreaterThanZero(int i){
         return i > 0 ? true : false;
     }
+
+    /**
+     * 判断是否等于零
+     * @param i
+     * @return
+     */
+    public static boolean equalsZero(int i) {
+        return i == 0;
+    }
+
+    /**
+     * 判断是否小于零
+     * @param i
+     * @return
+     */
+    public static boolean isLessThanZero(int i) {
+        return i > 0 ? true : false;
+    }
+
+
 }
